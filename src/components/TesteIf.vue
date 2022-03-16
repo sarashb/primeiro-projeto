19279544da7e7e6f22b7b38e335ef077e807f768
Sara Barros <sarasgnt@gmail.com>
2022-03-15 23:24:30 -0300

    testando o v-if

---
+++ src/components/TesteIf.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="teste-if">
+        <h1 v-if="podeVer">Teste</h1>
+        <h1 v-else>Teste2</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TesteIf',
+    data() {
+        return {
+            podeVer: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
